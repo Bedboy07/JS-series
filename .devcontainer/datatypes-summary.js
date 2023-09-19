251f9a7f5cd69debb1a17c386f7c5740b2d61d4d
Bedboy07 <138096077+Bedboy07@users.noreply.github.com>
2023-09-19 07:16:36 +0000

    Datatypes Summary

---
+++ .devcontainer/datatypes-summary.js
@@ -0,0 +1,29 @@
+//  Primitive
+
+// 7 Types : String, Number, Boolean, null, undefined, Symbol, BigInt
+
+const score = 100
+const scorevalue = 100.3
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId);
+
+const bigNumber = 876563563653557n
+
+
+
+// Reference (Non Primitive)
+
+// Array, Objects, Functions
+
+const heroes = [Superman.Batman,Ironman]
+let myObj = {
+    name: "Jatin",
+    age : 20,
+    
+}
