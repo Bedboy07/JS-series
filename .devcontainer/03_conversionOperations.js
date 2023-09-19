d62a83d022ef2fa6b1fa8fdfac5d0f41d92f3018
Bedboy07 <138096077+Bedboy07@users.noreply.github.com>
2023-09-19 06:21:13 +0000

    datatypes and operations

---
+++ .devcontainer/03_conversionOperations.js
@@ -0,0 +1,55 @@
+let score = null
+
+//console.log(typeof score);
+//console.log(typeof (score));
+
+let valueInnumber = Number(score)
+//console.log(typeof valueInnumber);
+//console.log(valueInnumber);
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1 ; false => 0
+
+let isLoggedin = ""
+let booleanisLoggedin = Boolean(isLoggedin)
+//console.log(booleanisLoggedin)
+
+// 1 => true; 0 => false
+// "" => false
+//  jatin => true
+
+let somenumber = 46
+// let stringnumber = String(somenumber)
+// console.log( typeof stringnumber)
+
+//****************Operations******************//
+
+let value = 3
+let negvalue = -value
+// console.log(negvalue)
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "hello"
+let str2 = " jatin" // given a space coz when we use concatention operator to give them a space bw them
+
+let str3 = str1+str2
+// console.log(str3);
+// console.log("1" + 2)
+// console.log(1+"2")
+// console.log("1"+ 2 + 2)
+// console.log(1+ 2 + "2")
+
+console.log(+true)
+console.log(+"")
+
+
+
+
+
